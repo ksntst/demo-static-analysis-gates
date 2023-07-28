16fe5a9dc95fa3585645c1521dd93cd49c264c6b
Bryan Lee <bryan.lee@datadoghq.com>
2023-07-28 17:51:02 -0400

    Update database.py

--- database.py
+++ database.py
@@ -13,3 +13,10 @@ def get_products(db_connection):
     cursor = db_connection.cursor()
     res = cursor.execute("SELECT id, title from products")
     return [Product(v[0], v[1]) for v in res]
+
+
+
+def get_product_by_id(db_connection, product_id):
+    cursor = db_connection.cursor()
+    res = cursor.execute(f"SELECT id, title from products WHERE id={product_id}")
+    return [Product(v[0], v[1]) for v in res]
