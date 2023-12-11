fc43529de984e3de6a3006d1a90ccd5f11e96f4c
Mark Azer <mark.azer@datadoghq.com>
2023-12-11 18:37:38 -0500

    Change function name

--- cipher.java
+++ cipher.java
@@ -1,9 +1,9 @@
 class MyClass {
 
-    public void test1() {
+    public void badCipher() {
         Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
         c.init(Cipher.ENCRYPT_MODE, k, iv);
         byte[] cipherText = c.doFinal(plainText);
     }
-    
+
 }
