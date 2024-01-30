084cf6dfd1b5366745cc8056a8e3be46fbad03ed
Mark Azer <mark.azer@datadoghq.com>
2024-01-30 18:06:40 -0500

    Add issues

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ import requests
 
 def get_product_from_api(product_id):
     try:
-        r = requests.get(f"https://api.provider.ext/products/{product_id}", timeout=5)
+        r = requests.get(f"https://api.provider.ext/products/{product_id}")
         return r.json()
     except requests.exceptions.RequestException as e:
         print(e)
