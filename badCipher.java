fa36e38f32a32474fbeb9fbc6e6ad4096438189a
Mark Azer <mark.azer@datadoghq.com>
2024-02-29 14:01:34 -0500

    Add Java examples

---
+++ badCipher.java
@@ -0,0 +1,14 @@
+class badCipher {
+
+    public void test1() {
+        Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        c.init(Cipher.ENCRYPT_MODE, k, iv);
+        byte[] cipherText = c.doFinal(plainText);
+    }
+
+    public void test2() {
+        Cipher c = javax.crypto.Cipher.getInstance("AES/CBC/PKCS5Padding");
+        c.init(Cipher.ENCRYPT_MODE, k, iv);
+        byte[] cipherText = c.doFinal(plainText);
+    }
+}
---
+++ hardcodedSecret.java
@@ -0,0 +1,7 @@
+class App {
+    private static void error1() {
+        Algorithm algorithm = Algorithm.HMAC256("secret");
+        Algorithm algorithm = Algorithm.HMAC512("secret");
+        Algorithm algorithm = Algorithm.HMAC384("secret");
+    }
+}
